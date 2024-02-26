5f5058d0bce75339df592a65e1ae2cf6b5eb6fc0
LeoDm123 <leo.meiners1@gmail.com>
2024-02-26 11:49:21 -0300

    pinterest redirect

--- src/components/landing/Dresscode.tsx
+++ src/components/landing/Dresscode.tsx
@@ -1,6 +1,8 @@
-import { Grid, Typography, Container } from "@mui/material";
+import { Grid, Typography, Container, Button } from "@mui/material";
 import "../../css/App.css";
 import EventIcon from "../../assets/icons/dresscode.png";
+import DressMale from "../../assets/imgs/dressHombre.jpg";
+import DressFemale from "../../assets/imgs/dressMujer.jpg";
 
 const Dresscode = () => {
   return (
@@ -21,8 +23,36 @@ const Dresscode = () => {
           sx={{ padding: 1 }}
           style={{ color: "#717264" }}
         >
-          ¡Como te sientas más cómodo!
+          Formal día.
         </Typography>
+        <Typography
+          variant="body1"
+          align="center"
+          sx={{ padding: 1 }}
+          style={{ color: "#717264" }}
+        >
+          ¡Te mostramos algunos modelos para que te inspires!
+        </Typography>
+        <Grid
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Button
+            href="https://ar.pinterest.com/search/pins/?q=vestimenta%20formal%20d%C3%ADa%20hombre%20casamiento&rs=typed"
+            target="blank"
+          >
+            <img src={DressMale} alt="Event Icon" className="dress-img" />
+          </Button>
+          <Button
+            href="https://ar.pinterest.com/search/pins/?q=vestimenta%20formal%20d%C3%ADa%20mujer%20casamiento&rs=typed"
+            target="blank"
+          >
+            <img src={DressFemale} alt="Event Icon" className="dress-img" />
+          </Button>
+        </Grid>
       </Grid>
     </Container>
   );
